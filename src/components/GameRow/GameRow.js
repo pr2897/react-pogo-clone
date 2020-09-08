c1dc2fe8aa0a3cc2806ca579751a541478df749a
PIYUSH RAJ <piyushrajkhg@gmail.com>
2020-09-09 00:44:27 +0530

    v1 - modified Carousel

--- src/components/GameRow/GameRow.js
+++ src/components/GameRow/GameRow.js
@@ -7,33 +7,36 @@ import "react-multi-carousel/lib/styles.css";
 import "./GameRow.css";
 
 const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5,
-  },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
     items: 3,
+    slidesToSlide: 3, // optional, default to 1.
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
     items: 2,
+    slidesToSlide: 2, // optional, default to 1.
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
     items: 1,
+    slidesToSlide: 1, // optional, default to 1.
   },
 };
 
-function GameRow({ title, GameData }) {
+function GameRow({ deviceType, title, GameData }) {
   return (
     <>
       <div className="gamerow">
         <h1>{title}</h1>
       </div>
       <div className="gamelist">
-        <Carousel responsive={responsive}>
+        <Carousel
+          showDots={true}
+          responsive={responsive}
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+        >
           {GameData.map(({ name, image, genre }) => (
             <GameCard name={name} image={image} genre={genre} />
           ))}
