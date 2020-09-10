f7eefcbe0b835d305a324b18a88a5afffe72aba3
PIYUSH RAJ <piyushrajkhg@gmail.com>
2020-09-10 13:54:43 +0530

    v1.0 - Responsive page

--- src/components/GameRow/GameRow.js
+++ src/components/GameRow/GameRow.js
@@ -9,8 +9,8 @@ import "./GameRow.css";
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3,
-    slidesToSlide: 3, // optional, default to 1.
+    items: 4,
+    slidesToSlide: 4, // optional, default to 1.
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
@@ -35,7 +35,7 @@ function GameRow({ deviceType, title, GameData }) {
           showDots={true}
           responsive={responsive}
           dotListClass="custom-dot-list-style"
-          itemClass="carousel-item-padding-40-px"
+          itemClass="carousel-item-padding-20-px card-width"
         >
           {GameData.map(({ name, image, genre }) => (
             <GameCard name={name} image={image} genre={genre} />
